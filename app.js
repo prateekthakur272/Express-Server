e7f0bdc6e51c55edf2ca569b87bf9b5b5c23fe77
Prateek Thakur <prateekthakur272@gmail.com>
2023-07-15 01:26:04 +0530

    serves files

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const path = require('path')
 const app = express()
 const port = 3000;
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./index.html'));
-    // res.sendFile(path.resolve(__dirname,'./index-style.css'));
-});
+app.use(express.static('./public'))
+
+// app.get('/', (req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'./public/index.html'));
+// });
 
 app.get('/about',(req,res)=>{
     res.send('About')
